27af92f437cf68836d1291d9138d93356b5e935a
mgadzhi <musaevgadzhi@gmail.com>
2015-05-08 02:19:23 +0300

    Hello world

--- project.clj
+++ project.clj
@@ -3,4 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [ring "1.4.0-beta1"]
+                 [http-kit "2.1.18"]
+                 [compojure "1.3.3"]
+                 [cheshire "5.4.0"]
+                 [korma "0.4.0"]]
+  :main word-keeper.core)
--- src/word_keeper/core.clj
+++ src/word_keeper/core.clj
@@ -1,6 +1,18 @@
-(ns word-keeper.core)
+(ns word-keeper.core
+  (:require [org.httpkit.server :refer [run-server]]
+            [ring.middleware.reload :as reload]
+            [compojure.handler :refer [site]]
+            [compojure.core :refer [defroutes GET POST]]
+            [compojure.route :refer [not-found]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn in-dev? [] true)
+
+(defroutes routes
+  (GET "/" [] "Hello, world!")
+  (not-found "Page not found"))
+
+(defn -main [& args]
+  (let [handler (if (in-dev?)
+                  (reload/wrap-reload (site #'routes))
+                  (site routes))]
+    (run-server handler {:port 8080})))
