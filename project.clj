592cb9a0bbe9b0c3467e3dd7484e075451e73ea8
gadzhi <musaevgadzhi@gmail.com>
2015-05-09 14:57:39 +0300

    DB setup script

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject word-keeper "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "JSON API for word keeper"
+  :url "https://github.com/mgadzhi/word-keeper"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
@@ -9,5 +9,9 @@
                  [compojure "1.3.3"]
                  [cheshire "5.4.0"]
                  [korma "0.4.0"]
-                 [org.postgresql/postgresql "9.2-1002-jdbc4"]]
+                 [org.postgresql/postgresql "9.2-1002-jdbc4"]
+                 [ragtime "0.3.8"]]
+  :plugins [[ragtime/ragtime.lein "0.3.8"]]
+  :ragtime {:migrations ragtime.sql.files/migrations
+            :database "jdbc:postgresql://localhost/wordkeeper?user=wordkeeper"}
   :main word-keeper.core)
